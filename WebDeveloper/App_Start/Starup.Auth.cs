e91c6d2f0feed6722c5cf337e5920a5042a4a624
WilfredoParedes <wilfredo.positiva@gmail.com>
2016-07-06 22:51:09 -0500

    Dataccess

---
+++ WebDeveloper/App_Start/Starup.Auth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+
+namespace WebDeveloper
+{
+    public partial class Starup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+               AuthenticationType= DefaultAuthenticationTypes.ApplicationCookie,
+
+            });
+        }
+    }
+}
--- WebDeveloper/Startup.cs
+++ WebDeveloper/Startup.cs
@@ -1,8 +1,9 @@
-﻿
-using LightInject;
+﻿using LightInject;
 using Microsoft.Owin;
 using Owin;
 using System.Reflection;
+using WebDeveloper.DataAccess;
+using WebDeveloper.Model;
 
 [assembly: OwinStartupAttribute(typeof(WebDeveloper.Startup))]
 namespace WebDeveloper
@@ -11,8 +12,8 @@ namespace WebDeveloper
     {
         public void Configuration(IAppBuilder app)
         {
+            ConfigureAuth(app);
             var container = new ServiceContainer();
-            //container.Register<IDataAccess<Client>, ClientData>();
             container.RegisterAssembly(Assembly.GetExecutingAssembly());
             container.RegisterAssembly("WebDeveloper.*.dll");
             container.RegisterControllers();
